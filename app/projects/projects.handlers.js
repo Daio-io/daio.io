437c9509eb0ea9eba4d164a9685c856dd2dac89f
Daveloper87 <dai.williams@bbc.co.uk>
2015-03-08 23:35:33 +0000

    Built project items using database data

--- app/projects/projects.handlers.js
+++ app/projects/projects.handlers.js
@@ -1,11 +1,15 @@
 'use strict';
+var Project = require('../../api/project/project.model');
 
 exports.getProj = function *() {
+    
+    let projects = yield Project.find().exec();
 
     yield this.render("projects", {
 
         title: 'Projects',
-        welcomeMessage: 'Project Page..'
+        welcomeMessage: 'Project Page..',
+        projects: projects
 
     });
 
