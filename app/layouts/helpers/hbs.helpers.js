7c4bc72afbef6939bfe9f7898f9bc379c0aa4469
Daveloper87 <dai.williams@bbc.co.uk>
2015-03-09 22:59:54 +0000

    First round handlebars label helper. Will be refactored

--- app/layouts/helpers/hbs.helpers.js
+++ app/layouts/helpers/hbs.helpers.js
@@ -1,21 +1,29 @@
 'use strict';
 
-// Set of handlebars helpers to be used on page
 var Handlebars = require('handlebars');
 
 module.exports = {
 
+    /**
+     * @name addLabels
+     * @function addLabels (Handlebars helper)
+     * @description Adds the correct color label tag to the project
+     * @returns {Handlebars.SafeString}
+     */
     addLabels: function () {
 
-        if (this.toUpperCase() === 'ANDROID') {
+        let platform = this.toUpperCase();
 
-            return new Handlebars.SafeString('<div class="ui green label">' + this + '</div>');
+        switch (platform) {
 
-        }
-
-        else {
-            
-            return new Handlebars.SafeString('<div class="ui label">' + this + '</div>');
+            case 'ANDROID':
+                return new Handlebars.SafeString('<div class="ui green tag label">' + this + '</div>');
+            case 'WEB':
+                return new Handlebars.SafeString('<div class="ui orange tag label">' + this + '</div>');
+            case 'NODE':
+                return new Handlebars.SafeString('<div class="ui red tag label">' + this + '</div>');
+            default:
+                return new Handlebars.SafeString('<div class="ui label">' + this + '</div>');
 
         }
 
