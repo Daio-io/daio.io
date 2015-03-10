401cb26b4861f5454207d5611a304036811ec372
Dai <dai.williams@bbc.co.uk>
2015-03-10 18:50:48 +0000

    Crated blog post pages

--- app/blog/blogposts.handlers.js
+++ app/blog/blogposts.handlers.js
@@ -3,7 +3,19 @@ var BlogPosts = require('../../api/blog/blog.model');
 
 exports.getBlog = function *() {
 
-    let blogPosts = yield BlogPosts.find().exec();
+    let blogPostsData = yield BlogPosts.find().exec();
+
+    let blogPosts = yield blogPostsData.map(function (found) {
+
+        return {
+            id: found._id,
+            title: found.title,
+            short: found.short,
+            full: found.full,
+            date: found.getDateCreated()
+        };
+
+    });
 
     yield this.render("blog", {
 
@@ -11,19 +23,20 @@ exports.getBlog = function *() {
         blogPosts: blogPosts
 
     });
-
 };
 
 exports.getBlogByTitle = function *() {
 
-    let title = this.params.title;
-
-    let blogPost = yield BlogPosts.find(title).exec();
+    let blogPostData = yield BlogPosts.find({ title: this.params.title }).exec();
+    
+    let blogPost = blogPostData[0];
+    
+    blogPost.date = blogPost.getDateCreated();
 
     yield this.render("blogPost", {
 
         title: 'Blog',
-        blogPosts: blogPost
+        blogPost: blogPost
 
     });
 
