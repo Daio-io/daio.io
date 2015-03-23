2a55e3cd393c3bd837f8d4a5b77eae0fb8633311
Daveloper87 <dai.williams@bbc.co.uk>
2015-03-23 21:49:13 +0000

    Minor clean up

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var koa = require('koa');
 var app = koa();
 var serve = require('koa-static');
 var settings = require('./app/conf/app.settings');
-var dbconnect = require('./api/conf/db/db.setup');
 
-dbconnect();
+// Connect to DB
+require('./api/conf/db/db.setup')();
 
 app.use(serve(__dirname + '/public', {maxage: settings.publicCache}));
 
