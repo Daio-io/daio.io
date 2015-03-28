1ac5d032bc7f396718cfc6b54953a43a0b8ecdb6
Daveloper87 <dai.williams@bbc.co.uk>
2015-03-28 10:17:37 +0000

    Added koa static caching package to replace koa-static

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var koa = require('koa');
 var app = koa();
-var serve = require('koa-static');
+var path = require('path');
+var staticCache = require('koa-static-cache');
 var settings = require('./app/conf/app.settings');
 
 // Connect to DB
 require('./api/conf/db/db.setup')();
 
-app.use(serve(__dirname + '/public', {maxage: settings.publicCache}));
+app.use(staticCache(path.join(__dirname + '/public'), {maxAge: settings.publicCache}));
 
 require('./app/conf/app.bootstrap')(app);
 require('./app/conf/app.routes')(app);
