a7d3a1ab14d2f05bfedd75a080eb97aa4197dc0a
Daveloper87 <dai.williams@bbc.co.uk>
2015-03-29 09:08:33 +0100

    Moved static serving to config file. Optomised server creation

--- app/conf/app.bootstrap.js
+++ app/conf/app.bootstrap.js
@@ -3,8 +3,12 @@ var hbsHelpers = require('../layouts/helpers/hbs.helpers');
 var settings = require('./app.settings');
 var koaBody = require('koa-body');
 var koaJson = require('koa-json');
+var path = require('path');
+var staticCache = require('koa-static-cache');
 
 module.exports = function (app) {
+
+    app.use(staticCache(path.resolve('./public'), { maxAge: settings.publicCache }));
 
     app.use(hbs(
         {
--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 var koa = require('koa');
 var app = koa();
-var path = require('path');
-var staticCache = require('koa-static-cache');
+var http = require('http');
+
 var settings = require('./app/conf/app.settings');
 
 // Connect to DB
 require('./api/conf/db/db.setup')();
 
-app.use(staticCache(path.join(__dirname + '/public'), {maxAge: settings.publicCache}));
-
 require('./app/conf/app.bootstrap')(app);
 require('./app/conf/app.routes')(app);
 
+http.createServer( app.callback()).listen(settings.port, function () {
+    console.log('Daio.io Started.. ');
 
-app.listen(settings.port, function () {
-    console.log('Started.. ');
 });
