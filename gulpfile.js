cb431c6d365c42de6eaba4a35dfa86cd651dc476
Dai <dai.williams@bbc.co.uk>
2015-08-19 23:20:06 +0100

    Refactor: removed deps and front end app files

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,5 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var del = require('del');
-var list = require('gulp-list');
 var mocha = require('gulp-mocha');
-
-gulp.task('clean', function () {
-    del([
-        'public/js/bundle.js'
-    ]);
-});
-
-gulp.task('help', function () {
-    return gulp.src('./build-tasks.json')
-        .pipe(list())
-
-});
-
-gulp.task('bundle', ['clean'], function () {
-    return browserify({entries: './front_app/app.js', debug: false})
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./public/js'))
-});
 
 gulp.task('api-tests', function () {
     return gulp.src('api/test/*.test.js')
@@ -31,4 +8,4 @@ gulp.task('api-tests', function () {
         }));
 });
 
-gulp.task('default', ['help'], function () {});
+gulp.task('default', function () {});
