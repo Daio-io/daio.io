7230e3a44ac1a1955a2bdd117c150ad5f3ed7600
Dai <dai.williams@bbc.co.uk>
2015-08-27 11:13:43 +0100

    Order projects by date

--- app/projects/projects.handlers.js
+++ app/projects/projects.handlers.js
@@ -3,7 +3,9 @@ const Project = require('../../api/project/project.model');
 
 exports.getProj = function *() {
     
-    let projects = yield Project.find().exec();
+    let projects = yield Project.find()
+      .sort('-_id')
+      .exec();
 
     yield this.render("projects", {
 
